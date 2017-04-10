9475c825cdad030a37c9d174da2bec002132a0f2
wsavd <tuve@rootfest.net>
2017-04-11 00:14:46 +0300

    passport jwt work

---
+++ index.js
@@ -0,0 +1,24 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/jwt');
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
